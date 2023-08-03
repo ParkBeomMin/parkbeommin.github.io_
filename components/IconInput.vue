d6a4d5917b9cd6055967690957e7f38a9e272afe
bmpark <bmpark@jinhak.com>
2023-08-03 17:51:21 +0900

    검색 인풋 컴포넌트 입력 이벤트

---
+++ components/IconInput.vue
@@ -0,0 +1,37 @@
+<template>
+    <label :for="id" class="flex flex-row w-full">
+        <input :id="id" type="text" class="flex-1" v-model="value" />
+        <IconFeather :data-feather="type" class="cursor-pointer" />
+    </label>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+    id: {
+        type: String,
+        default: 'search',
+    },
+    type: {
+        type: String,
+        default: 'search',
+    },
+    modelValue: {
+        type: String,
+        default: '',
+    },
+});
+// const { modelValue } = defineProps(['icon', 'modelValue']);
+
+const emit = defineEmits(['update:modelValue']);
+
+const value = computed({
+    get() {
+        return props.modelValue;
+    },
+    set(value) {
+        console.log(value);
+
+        emit('update:modelValue', value);
+    },
+});
+</script>
---
+++ components/icon/Feather.vue
@@ -0,0 +1,11 @@
+<template>
+    <i :data-feather="dataFeather"></i>
+</template>
+
+<script setup lang="ts">
+import feather from 'feather-icons';
+const { dataFeather } = defineProps<{ dataFeather: string }>();
+onMounted(() => {
+    feather.replace();
+});
+</script>
