d1e65cbf62a6d32e239aec0f6527b75cd36d1936
bmpark <bmpark@jinhak.com>
2023-08-03 17:46:50 +0900

    검색 인풋 컴포넌트 입력 이벤트

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,4 +1,8 @@
 <template>
-    <IconInput id="search" type="search" />
+    <IconInput id="search" type="search" v-model="test" />
     <PostList />
 </template>
+
+<script setup lang="ts">
+const test = ref('');
+</script>
