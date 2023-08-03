e399cf0afa71b89924b44910dce9ad136ece3ed1
ParkBeomMin <club20608@gmail.com>
2023-08-03 23:47:42 +0900

    top

--- components/Top.vue
+++ components/Top.vue
@@ -1,12 +1,24 @@
 <template>
-    <header class="fixed flex h-16 w-full">
+    <header class="fixed flex h-16 w-full z-50">
         <div class="flex flex-row max-w-5xl w-7/12 m-auto">
             <h1 class="flex-1">Beom Log</h1>
             <ul class="flex flex-row">
-                <li><nuxt-link to="/">Home</nuxt-link></li>
-                <li><nuxt-link to="/posts">Posts</nuxt-link></li>
-                <li><nuxt-link to="/AboutMe">AboutMe</nuxt-link></li>
+                <li
+                    v-for="(menu, i) of menuList"
+                    :key="`menu-${i}`"
+                    class="mr-2 transition hover:text-blue-400"
+                >
+                    <nuxt-link :to="menu.link">{{ menu.text }}</nuxt-link>
+                </li>
             </ul>
         </div>
     </header>
 </template>
+
+<script setup lang="ts">
+const menuList = reactive([
+    { text: "Home", link: "/" },
+    { text: "Posts", link: "/posts" },
+    { text: "AboutMe", link: "/aboutMe" },
+]);
+</script>
