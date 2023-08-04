e8c579781e67996fabf31f546687d9b8befc034d
ParkBeomMin <club20608@gmail.com>
2023-08-05 01:10:14 +0900

    검색 인풋창 css

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,7 +1,7 @@
 <template>
     <h3 class="mt-16 font-bold text-2xl text-center">포스트를 검색해보세요.</h3>
     <IconInput
-        class="mt-8 w-1/2 m-auto"
+        class="mt-8"
         id="search"
         type="search"
         :placeholder="randomPlaceholder"
