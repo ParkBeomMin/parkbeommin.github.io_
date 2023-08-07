d06f46e9b3e2e7f2099d2f87f5f6b508fa24c56c
bmpark <bmpark@jinhak.com>
2023-08-07 17:09:21 +0900

    meta

--- pages/index.vue
+++ pages/index.vue
@@ -4,4 +4,15 @@
     <PostList title="최신 포스트를 살펴보세요" :limit="5" :isShowCnt="false" />
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+useHead({
+    title: 'Beom Log',
+    meta: [
+        { name: 'description', content: '' },
+        { name: 'og:description', content: '' },
+        { name: 'keywords', content: '기술블로그' },
+        { name: 'og:image', content: '/assets/banner.png' },
+        { name: 'robots', content: 'index, follow' },
+    ],
+});
+</script>
--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,18 +1,15 @@
 <template>
     <h3 class="mt-16 font-bold text-2xl text-center">포스트를 검색해보세요.</h3>
-    <IconInput
-        class="mt-8"
-        id="search"
-        type="search"
-        :placeholder="randomPlaceholder"
-        v-model="postState.keyword"
-        @onClick="getPostList"
-    />
+    <IconInput class="mt-8" id="search" type="search" :placeholder="randomPlaceholder" v-model="postState.keyword" @onClick="getPostList" />
     <CategoryFilter class="mt-8" />
     <PostList />
 </template>
 
 <script setup lang="ts">
+useHead({
+    title: 'Beom Log | Posts',
+});
+
 const { postState, getPostList, resetFilter } = usePosts();
 
 const randomPlaceholder = computed(() => {
@@ -21,7 +18,7 @@ const randomPlaceholder = computed(() => {
 });
 
 onBeforeRouteLeave((to, from) => {
-    if (!to.fullPath.includes("/posts")) {
+    if (!to.fullPath.includes('/posts')) {
         resetFilter();
     }
 });
