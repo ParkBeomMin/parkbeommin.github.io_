3f8981077fa4d33dbee6e19e26d9029acad9332e
ParkBeomMin <club20608@gmail.com>
2023-08-08 21:03:43 +0900

    config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    ssr: false,
     modules: ["@nuxt/content", "@nuxtjs/tailwindcss", "nuxt-gtag"],
     content: {
         highlight: {
