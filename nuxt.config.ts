8693e98e19a8c38d6ed7a556e7ae0f13f97cf3b0
ParkBeomMin <club20608@gmail.com>
2023-08-08 20:55:06 +0900

    config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    target: "static",
-    router: {
-        base: "/parkbeommin.github.io/",
-    },
-    ssr: false,
-    devtools: { enabled: true },
     modules: ["@nuxt/content", "@nuxtjs/tailwindcss", "nuxt-gtag"],
     content: {
         highlight: {
@@ -20,14 +14,5 @@ export default defineNuxtConfig({
 
     gtag: {
         id: "G-44YWDK9DBE",
-    },
-    nitro: {
-        routeRules: {
-            "/": { static: true },
-            "/posts/**": { static: true },
-        },
-        prerender: {
-            routes: ["/sitemap.xml"],
-        },
     },
 });
