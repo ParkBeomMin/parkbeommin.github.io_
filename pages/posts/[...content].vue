7ec72a4b73ef47aa4a8bc4697878c47c962da120
bmpark <bmpark@jinhak.com>
2023-08-14 17:24:57 +0900

    반응형

--- pages/posts/[...content].vue
+++ pages/posts/[...content].vue
@@ -3,11 +3,11 @@
     <main ref="contentDoc" class="my-8">
         <ContentDoc v-slot="{ doc }">
             <div class="pb-4 border-b-2 border-black">
-                <h1 class="font-bold text-3xl">{{ doc.title }}</h1>
+                <h1 class="font-bold md:text-3xl text-xl">{{ doc.title }}</h1>
                 <span v-for="(category, i) of doc.categories?.split(' ')" :key="`category-${i}`" class="mr-2 text-blue-400">#{{ category }}</span>
                 <p class="mt-4 text-right">{{ getDate(doc.date) }}</p>
             </div>
-            <ContentRendererMarkdown class="mt-4 prose max-w-full" :value="convertImgPath(doc)" />
+            <ContentRendererMarkdown class="mt-4 prose max-w-full keepall" :value="convertImgPath(doc)" />
         </ContentDoc>
         <Share />
         <Comment />
@@ -45,7 +45,7 @@ const convertImgPath = (data: any) => {
     return JSON.parse(JSON.stringify(data).replace(/\/assets/g, '/images'));
 };
 
-const components = {};
+const components = { h2: '' };
 
 let contentDoc = ref();
 const { setIsShow, setX } = useCat();
@@ -83,35 +83,41 @@ onMounted(() => {
 </script>
 
 <style>
-h2 {
-    font-weight: bold !important;
-    font-size: 28px !important;
+@media (min-width: 768px) {
+    .prose h2 > a {
+        font-weight: bold !important;
+        font-size: 28px !important;
+    }
 }
-a {
+.prose h2 > a {
+    font-weight: bold !important;
+    font-size: 20px !important;
+}
+.prose a {
     text-decoration: none !important;
 }
 
-pre {
-    background-color: transparent !important;
-    border: 1px solid #1f2937 !important;
-    color: #1f2937 !important;
+.prose pre {
+    background-color: transparent;
+    border: 1px solid #1f2937;
+    color: #1f2937;
 }
 
-p > code {
-    border: 1px solid #1f2937 !important;
-    background-color: #1f2937 !important;
-    color: white !important;
+.prose p > code {
+    border: 1px solid #1f2937;
+    background-color: #1f2937;
+    color: white;
     border-radius: 4px;
 }
 
-p > code::before {
+.prose p > code::before {
     display: none;
 }
-p > code::after {
+.prose p > code::after {
     display: none;
 }
 
-img {
+.prose img {
     margin: auto;
 }
 </style>
