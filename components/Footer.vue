601aa5592a92620e5acc6855c68c29d31c8e35e3
bmpark <bmpark@jinhak.com>
2023-08-16 17:13:41 +0900

    푸터 변경

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="w-full h-36 absolute bottom-0 bg-blue-200 p-4 z-48">
-        <BlogTimer />
-        <SocialList />
+    <div class="w-full h-36 absolute bottom-0 border-t-2 p-4 z-48 text-center">
+        <BlogTimer class="justify-center" />
+        <SocialList class="justify-center" />
         <p class="mt-8">Copyright © 2023, All right reserved.</p>
     </div>
 </template>
